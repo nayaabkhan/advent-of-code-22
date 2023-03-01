8928dff49a20e3b9123b33b93eb5e1a2fa931610
Nayaabkhan Khan <nayaab@hey.com>
2023-03-01 16:19:55 +0530

    day1a: Use functional concepts

--- day1a/src/main.rs
+++ day1a/src/main.rs
@@ -1,22 +1,15 @@
 fn main() {
-    let mut calorie_tracker = 0;
-    let mut highest_so_far = 0;
+    let lines = include_str!("../input.txt")
+        .lines()
+        .map(|line| line.parse::<usize>().ok())
+        .collect::<Vec<_>>();
 
-    let lines = include_str!("../input.txt").lines();
+    let highest = lines
+        .split(|line| line.is_none())
+        .map(|group| group.iter().map(|v| v.unwrap()).sum::<usize>())
+        .max()
+        .unwrap();
 
-    for calorie in lines {
-        if calorie.trim().is_empty() {
-            if calorie_tracker > highest_so_far {
-                highest_so_far = calorie_tracker
-            }
-
-            calorie_tracker = 0
-        } else {
-            let calorie = calorie.parse::<u32>().unwrap();
-            calorie_tracker += calorie;
-        }
-    }
-
-    println!("{:?}", highest_so_far);
-    assert!(highest_so_far == 72602);
+    println!("{:?}", highest);
+    assert!(highest == 72602);
 }
