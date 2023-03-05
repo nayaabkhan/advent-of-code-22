0cc9ebca24b2ef4704a8bbb1d4f2a87c71b7932a
Nayaabkhan Khan <nayaab@hey.com>
2023-03-05 13:09:55 +0530

    day3a: Make logic functional

--- day3a/src/main.rs
+++ day3a/src/main.rs
@@ -4,30 +4,30 @@ use std::collections::HashSet;
 mod item;
 
 fn main() {
-    let mut total_priorities = 0;
-    let supplies = include_str!("../input.txt").lines();
+    let total_priorities: usize = include_str!("../input.txt")
+        .lines()
+        .map(|rucksack| -> usize {
+            let (compartment_1, compartment_2) = rucksack.split_at(rucksack.len() / 2);
 
-    for rucksack in supplies {
-        let (compartment_1, compartment_2) = rucksack.split_at(rucksack.len() / 2);
+            let compartment_1 = compartment_1
+                .bytes()
+                .map(Item::try_from)
+                .collect::<Result<HashSet<_>, _>>()
+                .expect("a lower or uppercase letter");
 
-        let compartment_1 = compartment_1
-            .bytes()
-            .map(Item::try_from)
-            .collect::<Result<HashSet<_>, _>>()
-            .expect("a lower or uppercase letter");
+            let compartment_2 = compartment_2
+                .bytes()
+                .map(Item::try_from)
+                .collect::<Result<HashSet<_>, _>>()
+                .expect("a lower or uppercase letter");
 
-        let compartment_2 = compartment_2
-            .bytes()
-            .map(Item::try_from)
-            .collect::<Result<HashSet<_>, _>>()
-            .expect("a lower or uppercase letter");
-
-        total_priorities += compartment_1
-            .intersection(&compartment_2)
-            .map(|i| i.priority())
-            .last()
-            .unwrap();
-    }
+            compartment_1
+                .intersection(&compartment_2)
+                .map(|i| i.priority())
+                .last()
+                .unwrap()
+        })
+        .sum();
 
     println!("{total_priorities}");
     assert!(total_priorities == 7785);
